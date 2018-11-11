94d8db839749a7df06bc2b21a225adf2fe303518
hkdnet <satoko.itse@gmail.com>
2018-11-11 17:12:09 +0900

    split

--- eval.go
+++ eval.go
@@ -39,7 +39,7 @@ func (ee *evalEnvironment) Unassign(name string) error {
 // Eval shows evaluated value
 func Eval(ast *AST) error {
 	var env evalEnvironment
-	n, err := eval(ast.Child, env)
+	n, err := eval(ast.Child, &env)
 	if err != nil {
 		return err
 	}
@@ -47,29 +47,75 @@ func Eval(ast *AST) error {
 	return nil
 }
 
-func eval(n *Node, env evalEnvironment) (*Node, error) {
+func eval(n *Node, env *evalEnvironment) (*Node, error) {
 	switch n.NodeType {
 	case True, False, Zero, FreeVariable:
 		return n, nil
 	case IF:
-		cond, err := eval(n.Children[0], env)
-		if err != nil {
-			return nil, err
-		}
-		if cond.NodeType == True {
-			return eval(n.Children[1], env)
-		}
-		if cond.NodeType == False {
-			return eval(n.Children[2], env)
-		}
-		return nil, errors.New("cond should be bool but not")
+		return evalIf(n, env)
+	case Apply:
+		return evalApply(n, env)
 	case Variable:
-		val, err := env.Lookup(n.Name)
-		if err != nil {
-			return nil, err
-		}
-		return val, nil
+		return evalVariable(n, env)
 	default:
 		return nil, errors.New("error")
 	}
+}
+
+func evalIf(n *Node, env *evalEnvironment) (*Node, error) {
+	cond, err := eval(n.Children[0], env)
+	if err != nil {
+		return nil, err
+	}
+	if cond.NodeType == True {
+		return eval(n.Children[1], env)
+	}
+	if cond.NodeType == False {
+		return eval(n.Children[2], env)
+	}
+	return nil, errors.New("cond should be bool but not")
+}
+
+func evalApply(n *Node, env *evalEnvironment) (*Node, error) {
+	var err error
+	l := n.Children[0]
+	r := n.Children[1]
+	l, err = eval(l, env)
+	if err != nil {
+		return nil, err
+	}
+	r, err = eval(r, env)
+	if err != nil {
+		return nil, err
+	}
+	if l.NodeType != Lambda { // cannot eval apply
+		return &Node{NodeType: Apply, Children: []*Node{l, r}}, nil
+	}
+	// l.NodeType == Lambda
+	def := l.Children[0]
+	body := l.Children[1]
+	assigned := def.Children[0]
+	env.Assign(assigned.Name, r)
+	body, err = eval(body, env)
+	if err != nil {
+		return nil, err
+	}
+	env.Unassign(assigned.Name)
+	if len(def.Children) == 1 {
+		return body, nil
+	}
+	def.Children = def.Children[1:] // tail
+	ret := &Node{
+		NodeType: Lambda,
+		Children: []*Node{def, body},
+	}
+	return ret, nil
+}
+
+func evalVariable(n *Node, env *evalEnvironment) (*Node, error) {
+	val, err := env.Lookup(n.Name)
+	if err != nil {
+		return nil, err
+	}
+	return val, nil
 }
