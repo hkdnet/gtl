078b78edf3c497b666ed7932aaa979a43daf06e0
hkdnet <satoko.itse@gmail.com>
2018-11-11 12:09:12 +0900

    Add Assign and Unassign

--- eval.go
+++ eval.go
@@ -5,7 +5,35 @@ import (
 	"fmt"
 )
 
+type assginment struct {
+	name  string
+	value *Node
+}
+
 type evalEnvironment struct {
+	assginments []assginment
+}
+
+func (ee *evalEnvironment) Assign(name string, val *Node) {
+	ee.assginments = append(ee.assginments, assginment{name, val})
+}
+
+func (ee *evalEnvironment) Lookup(name string) (*Node, error) {
+	for i := len(ee.assginments) - 1; i >= 0; i-- {
+		if ee.assginments[i].name == name {
+			return ee.assginments[i].value, nil
+		}
+	}
+	return nil, fmt.Errorf("missing assignment for %s", name)
+}
+func (ee *evalEnvironment) Unassign(name string) error {
+	for i := len(ee.assginments) - 1; i >= 0; i-- {
+		if ee.assginments[i].name == name {
+			ee.assginments = append(ee.assginments[:i], ee.assginments[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("missing unassignment target %s", name)
 }
 
 // Eval shows evaluated value
@@ -35,6 +63,12 @@ func eval(n *Node, env evalEnvironment) (*Node, error) {
 			return eval(n.Children[2], env)
 		}
 		return nil, errors.New("cond should be bool but not")
+	case Variable:
+		val, err := env.Lookup(n.Name)
+		if err != nil {
+			return nil, err
+		}
+		return val, nil
 	default:
 		return nil, errors.New("error")
 	}
--- eval_test.go
+++ eval_test.go
@@ -1,7 +1,42 @@
 package gtl
 
+import "testing"
+
 func ExampleEval() {
 	ast := &AST{Child: &Node{NodeType: True}}
 	Eval(ast)
 	// Output: &{True [] }
+}
+
+func Test_evalEnvironment(t *testing.T) {
+	var ee evalEnvironment
+
+	node1 := &Node{}
+	node2 := &Node{}
+	node3 := &Node{}
+	ee.Assign("a", node1)
+	ee.Assign("aa", node2)
+	v, err := ee.Lookup("a")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v != node1 {
+		t.Errorf("Lookup should return exact match")
+	}
+	ee.Assign("a", node3) // overwrite
+	v, err = ee.Lookup("a")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v != node3 {
+		t.Errorf("should return latest assignment")
+	}
+	err = ee.Unassign("aa")
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = ee.Lookup("aa")
+	if err == nil {
+		t.Error("Lookup for unassigned name should return error")
+	}
 }
