6a44da4315b2fe246e65f49c9d8383e510c436f8
hkdnet <satoko.itse@gmail.com>
2018-11-13 01:40:09 +0900

    Add tl-eval

---
+++ cmd/tl-eval/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/hkdnet/gtl"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "USAGE: %s FILENAME", os.Args[0])
+	}
+	filename := os.Args[1]
+
+	err := run(filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(filename string) error {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	source := string(b)
+
+	l := gtl.NewLexer(source)
+	var tokens []*gtl.Token
+	for l.HasNext() {
+		token, err := l.NextToken()
+		if err != nil {
+			return err
+		}
+		tokens = append(tokens, token)
+	}
+	ast, err := gtl.Parse(tokens)
+	if err != nil {
+		return err
+	}
+	result, err := gtl.Eval(ast)
+	if err != nil {
+		return err
+	}
+	showNode(result, "")
+	return nil
+}
+
+func showNode(n *gtl.Node, indent string) {
+	fmt.Printf("%s%s\n", indent, n.NodeType)
+	nextIndent := indent + "  "
+	for _, c := range n.Children {
+		showNode(c, nextIndent)
+	}
+}
